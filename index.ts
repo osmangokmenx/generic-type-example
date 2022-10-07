c8bb157f9d3b39486a68d4eea348221c35160948
osmangokmenx <52015838+osmangokmenx@users.noreply.github.com>
2022-10-07 15:38:28 +0300

    Generic type example

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+/* ------- Array -------  */
 const last = (arr: any[]) => {
   return arr[arr.length - 1];
 };
@@ -12,3 +13,40 @@ const last2 = <T>(arr: T[]) => {
 
 console.log(last2([1, 2, 3, 4, 5])); // return type is number
 console.log(last2(['1', '2', '3', '4', '5'])); // return type is string
+
+/* ------- Object -------  */
+
+const AddWorkerData = (people: { firstName: string; lastName: string }) => {
+  return {
+    ...people,
+    fullName: `${people.firstName} ${people.lastName}`,
+  };
+};
+
+const worker1 = AddWorkerData({ firstName: 'Osman', lastName: 'Gokmen' }); // it works
+const worker2 = AddWorkerData({
+  firstName: 'Osman',
+  lastName: 'Gokmen',
+  // age: 18,
+}); // age is not assignable
+
+const AddWorkerDataGenericType = <
+  T extends { firstName: string; lastName: string }
+>(
+  people: T
+) => {
+  return {
+    ...people,
+    fullName: `${people.firstName} ${people.lastName}`,
+  };
+};
+
+const genericWorker1 = AddWorkerDataGenericType({
+  firstName: 'Osman',
+  lastName: 'Gokmen',
+}); // it works
+const genericWorker2 = AddWorkerDataGenericType({
+  firstName: 'Osman',
+  lastName: 'Gokmen',
+  age: 18,
+}); // it works
